6afec7940096e933b1ec4cceb13719e261970955
Simon Kubiak <mail@simonkubiak.de>
2019-09-29 12:25:48 +0200

    extract exception messages to constants for easier testing

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -3,24 +3,28 @@
 namespace Simonkub\Laravel\Notifications\Sipgate\Exceptions;
 
 use Exception;
-use Throwable;
+use GuzzleHttp\Exception\GuzzleException;
 
 class CouldNotSendNotification extends Exception
 {
+    const NO_RECIPIENT = 'Could not find recipient for notification';
+    const NO_SMS_ID = 'Could not find SmsId';
+    const CONNECTION_FAILED = "Could not connect to sipgate: %s: %s";
+
     public static function noRecipient()
     {
-        return new static('Could not find recipient for notification');
+        return new static(self::NO_RECIPIENT);
     }
 
     public static function noSmsId()
     {
-        return new static('Could not find SmsId');
+        return new static(self::NO_SMS_ID);
     }
 
-    public static function connectionFailed(Throwable $exception)
+    public static function connectionFailed(GuzzleException $exception)
     {
         return new static(
-            "Could not connect to sipgate: {$exception->getCode()}: {$exception->getMessage()}"
+            sprintf(self::CONNECTION_FAILED, $exception->getCode(), $exception->getMessage())
         );
     }
 }
