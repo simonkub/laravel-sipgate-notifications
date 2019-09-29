41f62a30c0d08d202a54c5f915a5ceb88861eec5
Simon Kubiak <mail@simonkubiak.de>
2019-09-29 12:37:50 +0200

    remove unused no sms id method in CouldNotSendNotification exeption

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -8,17 +8,11 @@ use GuzzleHttp\Exception\GuzzleException;
 class CouldNotSendNotification extends Exception
 {
     const NO_RECIPIENT = 'Could not find recipient for notification';
-    const NO_SMS_ID = 'Could not find SmsId';
     const CONNECTION_FAILED = "Could not connect to sipgate: %s: %s";
 
     public static function noRecipient()
     {
         return new static(self::NO_RECIPIENT);
-    }
-
-    public static function noSmsId()
-    {
-        return new static(self::NO_SMS_ID);
     }
 
     public static function connectionFailed(GuzzleException $exception)
